550cd5debf236bda4e8cee23f77152b3db5592e4
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-05-14 17:57:12 -0400

    Finish chapter 3

--- sample_app/spec/requests/static_pages_spec.rb
+++ sample_app/spec/requests/static_pages_spec.rb
@@ -4,11 +4,45 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "has the content 'Sample App'" do
+    it "has the right title" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      expect(page).to have_selector('title',
+        :text => "Ruby on Rails Tutorial Sample App | Home")
+    end
+
+    it "should have the h1 help" do
+      visit '/static_pages/home'
+      expect(page).to have_selector('h1', :text => "Sample App")
     end
 
   end
 
+  describe "Help page" do
+
+    it "has the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_selector('title',
+        :text => "Ruby on Rails Tutorial Sample App | Help")
+    end
+
+    it "has the h1 'Sample App'" do
+      visit '/static_pages/help'
+      expect(page).to have_selector('h1', :text => 'Get a Halp')
+    end
+
+  end
+
+  describe "About page" do
+
+    it "has the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_selector('title',
+        :text => "Ruby on Rails Tutorial Sample App | About Us")
+    end
+
+    it "has the h1 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_selector('h1', :text => 'About Us')\
+    end
+  end
 end
